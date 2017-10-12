e6cd1cdf854cbe5275df2e3bf79df1d1d4e06445
Mitesh Shah <mitesh@mokko.io>
2017-10-12 12:07:38 +0530

    Updates

--- src/actions/device.js
+++
@@ -1,4 +0,0 @@
-export const updateDevice = (type, dispatch) => dispatch({
-  type: "CHANGE_DEVICE",
-  payload: type
-})
--- src/components/Common/Logo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import logo from '../../images/logo.svg'
-
-const Logo = () =>
-  <img src={logo} alt="TypeSetter" />
-  ;
-  export default Logo;
